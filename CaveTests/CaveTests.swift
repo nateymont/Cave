20d45073bce57d73dfc992fda384138a06c5f355
Nate <nate@cave.bet>
2024-12-02 22:16:27 -0800

    Initial Commit

---
+++ CaveTests/CaveTests.swift
@@ -0,0 +1,17 @@
+//
+//  CaveTests.swift
+//  CaveTests
+//
+//  Created by Nate Montgomery on 12/2/24.
+//
+
+import Testing
+@testable import Cave
+
+struct CaveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
